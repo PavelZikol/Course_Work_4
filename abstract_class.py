3ede5882717414c03b904815ceaef0f361326190
PavelZikol <142353611+PavelZikol@users.noreply.github.com>
2023-10-16 23:15:51 +0400

    abstract_class

---
+++ abstract_class.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+class CityAndVacancies(ABC):
+    @abstractmethod
+    def get_city_id(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+class JsonHelper(ABC):
+    @abstractmethod
+    def save_to_json(self):
+        pass
+
+    @abstractmethod
+    def get_json(self):
+        pass
+
+    @abstractmethod
+    def delete_from_json(self):
+        pass
